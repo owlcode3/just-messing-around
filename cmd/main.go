4aa324e2f8e4c93f779418f2008435842d3e8018
owlcode3 <akoladeo890@yahoo.com>
2025-11-25 00:55:28 +0100

    added times feature

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/owlcode3/jma/add"
+	"github.com/owlcode3/jma/times"
 )
 
 func main() {
 	fmt.Println(add.Add(800, 900))
+	fmt.Println(times.Times(250, 349))
 }
---
+++ times/times.go
@@ -0,0 +1,5 @@
+package times
+
+func Times(a, b int) int {
+	return a * b
+}
